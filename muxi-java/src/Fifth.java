98090bf282f8749cde1ee02741bce82d3d68ad09
liuyuxin-cloud <2590177089@qq.com>
2020-11-10 23:04:36 +0800

    11.10

--- muxi-java/src/Fifth.java
+++ muxi-java/src/Fifth.java
@@ -2,8 +2,7 @@
  *该类中应包含String类型的成员属性brand 商标 和color 颜色
  * 还应包含成员方法run 行驶在控制台显示“我已经开动了”
  * 和showInfo 显示信息 在控制台显示商标和颜色
- * 并编写构造方法初始化其成员属性。
- * 编写Car 小汽车 类继承于Vehicles类
+  * 编写Car 小汽车 类继承于Vehicles类
  *    增加int型成员属性seats 座位
  *    还应增加成员方法showCar
  *    在控制台显示小汽车的信息 并编写构造方法。
@@ -13,4 +12,51 @@
  *    在控制台显示卡车的信息 并编写构造方法。 在main方法中测试以上各类。
 */
  public class Fifth {
+  public static void main(String[] args){
+   Car n1 = new Car("bmw","yellow",5);
+   n1.run();
+   n1.showInfo();
+   n1.showCar();
+
+
+   Truck n2 = new Truck("bmw","white",12);
+   n2.run();
+   n2.showInfo();
+   n2.showTrack();
+  }
+}
+class Vehicles{
+  String brand;
+  String colour;
+
+  public Vehicles(String b,String c){
+   brand = b;
+   colour = c;
+  }
+  public void run(){
+   System.out.println("我已经开动了");
+  }
+  public void showInfo(){
+   System.out.println( brand + " "+ colour );
+  }
+}
+class Car extends Vehicles{
+  int seats;
+  public Car(String b,String c,int n){
+   super(b,c);
+   seats = n;
+  }
+  public void showCar(){
+   System.out.println( brand + " " + colour +  "座位数： " + seats );
+  }
+}
+class Truck extends Vehicles{
+  float load;
+  public Truck(String b,String c,float n){
+   super(b,c);
+   load = n;
+  }
+  public void showTrack(){
+   System.out.println( brand + " " + colour +  "载重为： " + load );
+  }
 }
---
+++ 核心技术/Chapter4/src/equals/Manager.java
@@ -0,0 +1,29 @@
+package equals;
+
+import java.util.Objects;
+
+public class Manager extends Employee {
+    private double bonus;
+    public Manager(String name,double salary,int year,int month,int day);{
+        super(name,salary,year,month,day);
+        bonus = 0;
+    }
+    public double getSalary(){
+        double baseSalary = super.getSalary();
+        return baseSalary + bonus;
+    }
+    public void setBonus(double b) {
+        bonus = b;
+    }
+    public boolean equals(Object otherObject){
+        if(!super.equals(otherObject)) return false;
+        var other = (Manager)otherObject;
+        return bonus == other.bonus;
+    }
+    public int hashCode(){
+        return java.util.Objects.hash(super.hashCode(),bonus);
+    }
+    public String toString(){
+        return super.toString() + "[bonus=" + bonus + "]";
+    }
+}
