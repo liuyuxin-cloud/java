c9d7e7ff8433412d868ba0bc088a1e130ecebc00
liuyuxin-cloud <2590177089@qq.com>
2020-12-12 00:41:28 +0800

    12.11

--- 核心技术/Chapter5and6/src/inheritance/Employee.java
+++ 核心技术/Chapter5and6/src/inheritance/Employee.java
@@ -6,7 +6,7 @@ public class Employee {
     private String name;
     private double salary;
     private LocalDate hireDay;
-
+    private int bonus;
     public Employee(String name,double salary,int year,int month,int day){
         this.name = name;
         this.salary = salary;
@@ -30,5 +30,13 @@ public class Employee {
     public void raiseSalary(double byPercent){
         double raise = salary * byPercent / 100;
         salary += raise;
+    }
+
+    public int getBonus() {
+        return bonus;
+    }
+
+    public void setBonus(int bonus) {
+        this.bonus = bonus;
     }
 }
